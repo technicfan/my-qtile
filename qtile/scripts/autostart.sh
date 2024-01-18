ba5c824fcf850e0824b9465aa91956581a209f3e
Technicfan <lenni.ahrens@web.de>
2024-01-18 19:11:17 +0100

    cleanup

--- qtile/scripts/autostart.sh
+++ qtile/scripts/autostart.sh
@@ -19,37 +19,18 @@ function run {
 #autorandr horizontal
 
 #change your keyboard if you need it
-#setxkbmap -layout be
+#setxkbmap -layout de
 
 keybLayout=$(setxkbmap -v | awk -F "+" '/symbols/ {print $2}')
 
-#if [ $keybLayout = "be" ]; then
-#  cp $HOME/.config/qtile/config-azerty.py $HOME/.config/qtile/config.py
-#fi
-
-#autostart ArcoLinux Welcome App
-#run dex $HOME/.config/autostart/arcolinux-welcome-app.desktop &
-
 #Some ways to set your wallpaper besides variety or nitrogen
-#feh --bg-fill /usr/share/backgrounds/archlinux/arch-wallpaper.jpg &
-#feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
 feh --bg-fill "/home/technicfan/.config/qtile/wallpapers/Arcolinux-text-dark-rounded-1080p.png" &
-#wallpaper for other Arch based systems
-#feh --bg-fill /usr/share/archlinux-tweak-tool/data/wallpaper/wallpaper.png &
-#start the conky to learn the shortcuts
-#(conky -c $HOME/.config/qtile/scripts/system-overview) &
-
-#start sxhkd to replace Qtile native key-bindings
-#run sxhkd -c ~/.config/qtile/sxhkd/sxhkdrc &
-
 
 #starting utility applications at boot time
-#run variety &
 run nm-applet &
 #run pamac-tray &
 run xfce4-power-manager &
 #numlockx on &
-#blueberry-tray &
 blueman-applet &
 picom &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
@@ -65,18 +46,3 @@ polychromatic-cli -o brightness -p 50 &
 polychromatic-cli --dpi 2600 &
 
 .config/qtile/scripts/mouse.sh &
-
-#starting user applications at boot time
-#run volumeicon &
-#run discord &
-#nitrogen --restore &
-#run caffeine -a &
-#run vivaldi-stable &
-#run firefox &
-#run thunar &
-#run dropbox &
-#run insync start &
-#run spotify &
-#run atom &
-#run telegram-desktop &
-#run /usr/bin/octopi-notifier &
