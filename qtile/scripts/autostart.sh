63eca34f939ee119182a1274015a179d8657f8c5
Technicfan <lenni.ahrens@web.de>
2024-01-21 00:13:22 +0100

    add activitywatch

--- qtile/scripts/autostart.sh
+++ qtile/scripts/autostart.sh
@@ -26,6 +26,7 @@ picom &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
 /usr/lib/kdeconnectd &
+aw-qt &
 polychromatic-tray-applet &
 nextcloud &
 caffeine &
