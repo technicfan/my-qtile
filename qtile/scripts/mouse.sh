3de91a52b80926d50b6a91e3ed0ff20c13054045
Technicfan <lenni.ahrens@web.de>
2024-01-23 21:03:01 +0100

    add mouse name variable

--- qtile/scripts/mouse.sh
+++ qtile/scripts/mouse.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+# set mouse name here:
+mouse="Razer Razer Basilisk V3"
+
 for i in 1 2 
 do
-    id=$(xinput --list | grep "↳ Razer Razer Basilisk V3" | grep -n "pointer" | grep -no "id=.." | grep -Po "$i:id=\K..")
+    id=$(xinput --list | grep "↳ $mouse" | grep -n "pointer" | grep -no "id=.." | grep -Po "$i:id=\K..")
     xinput --set-prop "$id" "Coordinate Transformation Matrix" 0.4 0 0 0 0.4 0 0 0 1
 done
