32396bace34d8d18ec0358e5c595aa22f721d02f
Technicfan <lenni.ahrens@web.de>
2024-01-27 16:51:24 +0100

    dirty fix for screen2 wallpaper

--- qtile/scripts/two-screens.sh
+++ qtile/scripts/two-screens.sh
@@ -5,3 +5,14 @@ xrandr --output DP-4 --auto --output HDMI-0 --auto --right-of DP-4
 cd /home/technicfan/.config/qtile/screens
 
 touch 2 && rm 1
+
+# Arcolinux-text-dark-rounded-1080p.png
+# Archlinux-text-dark-rounded-1080p.png
+# linux.png
+# retro-arco.png
+
+# Arcolinux
+feh --bg-fill "/home/technicfan/.config/qtile/wallpapers/Arcolinux-text-dark-rounded-1080p.png" &
+
+# Archlinux
+#feh --bg-fill "path/to/wallpaper" &
