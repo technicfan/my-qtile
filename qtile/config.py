a7764f572e5c600eae951c4d5d8c5e2cb03806fa
Technicfan <lenni.ahrens@web.de>
2024-02-02 21:28:50 +0100

    cleanup

--- qtile/config.py
+++ qtile/config.py
@@ -50,14 +50,6 @@ def minimize_all(qtile):
     for win in qtile.current_group.windows:
         if hasattr(win, "toggle_minimize"):
             win.toggle_minimize()
-            
-#@lazy.function
-#def maximize_by_switching_layout(qtile):
-#    current_layout_name = qtile.current_group.layout.name
-#    if current_layout_name == "monadtall":
-#        qtile.current_group.layout = "max"
-#    elif current_layout_name == "max":
-#        qtile.current_group.layout = "monadtall"
 
 # Dmenu theme
 dmenu_theme = {
