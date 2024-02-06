6a3d1d9b76fa11dbe34d099f1ee5080e877daeb4
Technicfan <lenni.ahrens@web.de>
2024-02-06 19:27:42 +0100

    change sensi again

--- qtile/scripts/mouse.sh
+++ qtile/scripts/mouse.sh
@@ -6,5 +6,5 @@ mouse="Razer Razer Basilisk V3"
 for i in 1 2 
 do
     id=$(xinput --list | grep "↳ $mouse" | grep -n "pointer" | grep -no "id=.." | grep -Po "$i:id=\K..")
-    xinput --set-prop "$id" "Coordinate Transformation Matrix" 0.5 0 0 0 0.5 0 0 0 1
+    xinput --set-prop "$id" "Coordinate Transformation Matrix" 0.45 0 0 0 0.45 0 0 0 1
 done
