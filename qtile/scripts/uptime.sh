d2618729da3fe0c2d6c7c3d3dc68ebacdbccb37b
Technicfan <lenni.ahrens@web.de>
2024-02-07 22:15:37 +0100

    formating

--- qtile/scripts/uptime.sh
+++ qtile/scripts/uptime.sh
@@ -4,19 +4,26 @@ d=$(awk '{print int($1/86400)}' /proc/uptime)
 h=$(awk '{print int($1/3600)}' /proc/uptime)
 m=$(awk '{print int(($1%3600)/60)}' /proc/uptime)
 
-if [[ $d -eq 0 && $h -eq 0 && $m < 1 ]]; then
+if [[ $d = 0 && $h = 0 && $m < 1 ]]
+then
     printf "unter 1min"
-elif [[ $d -eq 0 && $h -eq 0 ]]; then
+elif [[ $d = 0 && $h = 0 ]]
+then
     printf $m"min"
-elif [[ $d -eq 0 && $m -eq 0 ]]; then
+elif [[ $d = 0 && $m = 0 ]]
+then
     printf $h"h"
-elif [[ $h -eq 0 && $m -eq 0 ]]; then
+elif [[ $h = 0 && $m = 0 ]]
+then
     printf $d"d"
-elif [[ $d -eq 0 ]]; then
+elif [ $d = 0 ]
+then
     printf $h"h "$m"min"
-elif [[ $h -eq 0 ]]; then
+elif [ $h = 0 ]
+then
     printf $d"d "$m"min"
-elif [[ $m -eq 0 ]]; then
+elif [ $m = 0 ]
+then
     printf $d"d "$h"h"
 else
     printf $d"d "$h"h "$m"min"
