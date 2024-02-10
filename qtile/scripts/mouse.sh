31ebaee8e0efe739e0391835d52b275ce7c4113c
Technicfan <lenni.ahrens@web.de>
2024-02-10 16:44:50 +0100

    add sensi variable

--- qtile/scripts/mouse.sh
+++ qtile/scripts/mouse.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-# set mouse name here:
+# set mouse name here
 mouse="Razer Razer Basilisk V3"
+# set sensitivity here
+sensi="0.45"
 
 for i in 1 2 
 do
     id=$(xinput --list | grep "↳ $mouse" | grep -n "pointer" | grep -no "id=.." | grep -Po "$i:id=\K..")
-    xinput --set-prop $id "Coordinate Transformation Matrix" 0.45 0 0 0 0.45 0 0 0 1
+    xinput --set-prop $id "Coordinate Transformation Matrix" $sensi 0 0 0 $sensi 0 0 0 1
 done
