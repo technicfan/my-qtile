decd76d6673626d556d0be92d679729f3cdfcdcd
Technicfan <lenni.ahrens@web.de>
2024-02-11 22:26:30 +0100

    change size of monitor scratchpad window

--- qtile/config.py
+++ qtile/config.py
@@ -182,7 +182,7 @@ for i in groups:
 groups.append(
     ScratchPad("scratchpad", [ DropDown("mixer", "pavucontrol", width=0.5, height=0.5, x=0.25, y=0.1, opacity=1, on_focus_lost_hide=False),
                                DropDown("term", myTerm, width=0.5, height=0.5, x=0.25, y=0.1, opacity=1, on_focus_lost_hide=False),
-                               DropDown("monitor", "gnome-system-monitor", width=0.5, height=0.6, x=0.25, y=0.1, opacity=1, on_focus_lost_hide=False),
+                               DropDown("monitor", "gnome-system-monitor", width=0.55, height=0.6, x=0.225, y=0.1, opacity=1, on_focus_lost_hide=False),
                              ]),
 )
 
