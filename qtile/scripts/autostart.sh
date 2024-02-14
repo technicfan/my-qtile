9c7274858bdbebdee6c20ae4583c57e1573e5870
Technicfan <lenni.ahrens@web.de>
2024-02-14 15:56:38 +0100

    add clipcat

--- qtile/scripts/autostart.sh
+++ qtile/scripts/autostart.sh
@@ -30,6 +30,7 @@ picom -b --config .config/picom/picom.conf &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
 /usr/lib/kdeconnectd &
+clipcatd &
 aw-qt &
 polychromatic-tray-applet &
 nextcloud &
