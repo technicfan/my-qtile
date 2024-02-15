b23f0f7f60111ef5fefb5a03af257663ef8e483c
Technicfan <lenni.ahrens@web.de>
2024-02-15 16:56:29 +0100

    normal

--- qtile/config.py
+++ qtile/config.py
@@ -201,8 +201,8 @@ def ReplaceWindowName(text):
 ### COLORSCHEME ###
 # Colors are defined in a separate 'colors.py' file.
 
-#colors = colors.Technicfan
-colors = colors.TechnicfanClean
+colors = colors.Technicfan
+#colors = colors.TechnicfanClean
 
 ### LAYOUTS ###
 
