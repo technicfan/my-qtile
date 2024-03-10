1871814285e1e59117acd956034350dc93c803b6
Technicfan <lenni.ahrens@web.de>
2024-03-10 02:01:01 +0100

    add option to set default wallpaper in wallpaper script

--- qtile/scripts/dmenu-wallpaper.sh
+++ qtile/scripts/dmenu-wallpaper.sh
@@ -2,11 +2,21 @@
 
 main()
 {
+    local DMENU_WIDTH=" -z 400 -p"
+
     local choice=$(ls ~/.config/qtile/wallpapers/ | $DMENU $DMENU_WIDTH 'Wallpapers:')
     if [[ -n $choice && $(echo -e "No\nYes" | $DMENU $DMENU_WIDTH "Choose \"$choice\"?") = "Yes" ]]
     then
         feh --bg-fill ~/.config/qtile/wallpapers/$choice
         notify-send "\"$choice\" is your new wallpaper"
+
+        if [[ $(echo -e "No\nYes" | $DMENU $DMENU_WIDTH "Set \"$choice\" as default?") = "Yes" ]]
+        then
+            sed -i "s/wallpapers\/.*/wallpapers\/$choice\"/g" ~/.config/qtile/scripts/screens.sh
+            notify-send "\"$choice\" set as default"
+        else
+            exit 1
+        fi
     else
         exit 1
     fi
--- qtile/scripts/screens.sh
+++ qtile/scripts/screens.sh
@@ -16,7 +16,7 @@ set_state()
 
 wallpaper()
 {
-    feh --bg-fill ".config/qtile/wallpapers/motivation.png"
+    feh --bg-fill ".config/qtile/wallpapers/destroy-big-tech.png"
 }
 
 one()
