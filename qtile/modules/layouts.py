ded5a229117e83c2bf2dd46f37c57dfb24a61cbe
Technicfan <lenni.ahrens@web.de>
2024-03-18 17:11:52 +0100

    make changes to spiral layout + qtiles add copyright notice

--- qtile/modules/layouts.py
+++ qtile/modules/layouts.py
@@ -1,3 +1,22 @@
+# Copyright (c) 2022 elParaguayo
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 # _____ _____ ____ _   _ _   _ ___ ____ _____ _    _   _    ____ ____  _____    _  _____ ___ ___  _   _   _
 #|_   _| ____/ ___| | | | \ | |_ _/ ___|  ___/ \  | \ | |  / ___|  _ \| ____|  / \|_   _|_ _/ _ \| \ | | | |
 #  | | |  _|| |   | |_| |  \| || | |   | |_ / _ \ |  \| | | |   | |_) |  _|   / _ \ | |  | | | | |  \| | | |
@@ -6,21 +25,24 @@
 
 from libqtile import layout
 from libqtile.config import Match
+# custom spiral
+from spiral import Spiral
 
 from colors import colors
 
 ### LAYOUTS ###
 layouts = [
-    layout.Spiral(
+    Spiral(
         main_pane = "left",
-        clockwise = True,
+        i3_style = True,
         new_client_position = "after_current",
         ratio = 0.5,
         ratio_increment = 0,
         border_width = 2,
         margin = [4, 8, 8, 8],
         border_focus = colors[1],
-        border_normal = colors[3]
+        border_normal = colors[3],
+        order = []
     ),
     layout.Max(),
 ]
