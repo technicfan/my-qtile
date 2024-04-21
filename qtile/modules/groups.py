8be2f8704a2ed4f81becead2f3f2c70f40adf97f
Technicfan <lenni.ahrens@web.de>
2024-04-21 03:10:48 +0200

    update

--- qtile/modules/groups.py
+++ qtile/modules/groups.py
@@ -62,6 +62,6 @@ dgroups_app_rules = [
     Rule(Match(wm_class=re.compile(r"^(spotify)$")), group="0"),
     Rule(Match(wm_class=re.compile(r"^(VirtualBox\ Machine|virt\-manager)$")), group="9"),
     Rule(Match(title=re.compile(r"^(VirtualBoxVM)$")), group="9"),
-    Rule(Match(wm_class=re.compile(r"^(discord|signal|web.whatsapp.com|chat.openai.com)$")), group="8"),
+    Rule(Match(wm_class=re.compile(r"^(discord|signal|web.whatsapp.com)$")), group="8"),
     Rule(Match(wm_class=re.compile(r"^(vscodium|delphi32.exe)$")), group="7"),
 ]
