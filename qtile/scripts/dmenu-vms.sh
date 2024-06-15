e72de654aea3aa92650a7cd47185f7e76bab9d5e
Technicfan <lenni.ahrens@web.de>
2024-06-15 18:20:36 +0200

    add vmware windows 11

--- qtile/scripts/dmenu-vms.sh
+++ qtile/scripts/dmenu-vms.sh
@@ -7,18 +7,24 @@ main()
         local vms[$i]=$(ls -m ~/VirtualBox\ VMs | awk -F ", " '{ print $(shell_var='"$i"') }' )
     done
 
-    local choice=$(printf '%s\n' "${vms[@]}" | $DMENU 'Choose VM:')
+    local choice=$(printf '%s\n' "VMWare Windows 11" "${vms[@]}" | $DMENU 'Choose VM:')
 
     if [[ -n $choice && "$(echo -e "No\nYes" | $DMENU "Start VM \"$choice\"?")" == "Yes" ]]
     then
-        local file=$(ls -1 "$HOME/VirtualBox VMs/$choice/" | grep .vbox | grep -n "" | grep 1: | awk -F ":" '{ print $2 }')
-        local vm=$(cat "$HOME/VirtualBox VMs/$choice/$file" | grep "Machine uuid" | awk -F "=" '{ print $3}' | awk -F "\"" '{ print $2 }')
-        if [[ -n $vm ]]
+        if [[ "$choice" = "VMWare Windows 11" ]]
         then
-            notify-send "starting VM \"$choice\""
-            virtualboxvm --startvm "$vm"
+            notify-send "starting \"$choice\""
+            vmrun -vp "$(cat pass)" -T ws start $HOME/vmware/Windows\ 11\ x64/Windows\ 11\ x64.vmx
         else
-            notify-send "VM \"$choice\" not found"
+            local file=$(ls -1 "$HOME/VirtualBox VMs/$choice/" | grep .vbox | grep -n "" | grep 1: | awk -F ":" '{ print $2 }')
+            local vm=$(cat "$HOME/VirtualBox VMs/$choice/$file" | grep "Machine uuid" | awk -F "=" '{ print $3}' | awk -F "\"" '{ print $2 }')
+            if [[ -n $vm ]]
+            then
+                notify-send "starting VM \"$choice\""
+                virtualboxvm --startvm "$vm"
+            else
+                notify-send "VM \"$choice\" not found"
+            fi
         fi
     else
         exit 0
